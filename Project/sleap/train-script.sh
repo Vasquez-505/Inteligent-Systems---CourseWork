708d512e09169f6eb91f0638269a831ce8c820eb
Vasquez-505 <pepe.vasques10@gmail.com>
2025-10-21 13:03:22 +0100

    Add files via upload

---
+++ Project/sleap/train-script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Auto-generated: drosophila_unet_64_251020_093034
+echo "Starting SLEAP training: drosophila_unet_64_251020_093034"
+sleap-train single_instance.yaml colab.pkg.slp --first-gpu
